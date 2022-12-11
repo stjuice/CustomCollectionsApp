62d23e62ba8681cc7195714f2c6766f3ad64b27c
Viktoriia Tryfonova <v.tryfonova@sana-commerce.com>
2022-12-11 17:45:38 +0200

    Fix build - add parenthesis

--- ConsoleApp1/ArrayListGenericSingleLinked.cs
+++ ConsoleApp1/ArrayListGenericSingleLinked.cs
@@ -50,119 +50,120 @@ namespace CustomCollectionsApp
                     current = current.Next;
         }
 
-    public void AddItem(T item)
-    {
-        var newNode = new Node(item);
-        InsertAfter(tail, newNode);
-    }
-
-    public void AddFirst(T item)
-    {
-        var newNode = new Node(item);
-        if (head is null)
-            head = tail = newNode;
-        else
-            head = newNode;
-    }
-
-    public void RemoveNode(T item)
-    {
-        Node current = head;
-        if (current == null)
-            return;
-
-        if (current.Value.Equals(item))
+        public void AddItem(T item)
         {
-            if (current.Next != null)
-                current = current.Next;
-            else
-                current = null;
-
-            head = current;
-        }
-        else
-        {
-            while (current.Next != null && !current.Next.Value.Equals(item))
-            {
-                current = current.Next;
-            }
-
-            if (current.Next != null && current.Next.Value.Equals(item))
-                current.Next = current.Next.Next;
-        }
-    }
-
-    public bool Contains(T value)
-    {
-        return Find(value) != null;
-    }
-
-    public IEnumerable<Node> Iterate()
-    {
-        var currentNode = head;
-
-        while (currentNode != null)
-        {
-            yield return currentNode;
-
-            currentNode = currentNode.Next;
-        }
-    }
-
-    public void AddLast(T item)
-    {
-        var newNode = new Node(item);
-        InsertLast(newNode);
-    }
-
-    private void InsertLast(Node newNode)
-    {
-        if (tail is null)
-        {
-            head = tail = newNode;
-        }
-        else
-        {
+            var newNode = new Node(item);
             InsertAfter(tail, newNode);
         }
-    }
 
-    private void InsertAfter(Node existingNode, Node newNode)
-    {
-        if (existingNode is null)
-            head = newNode;
-        else
-            existingNode.Next = newNode;
-
-        tail = newNode;
-    }
-
-    private void InsertNode(Node newNode)
-    {
-        if (head != null)
+        public void AddFirst(T item)
         {
-            head.Next = newNode;
+            var newNode = new Node(item);
+            if (head is null)
+                head = tail = newNode;
+            else
+                head = newNode;
         }
-        else
-        {
-            head = newNode;
-        }
-    }
 
-    private Node Find(T searchedValue)
-    {
-        var currentNode = head;
-
-        while (currentNode != null)
+        public void RemoveNode(T item)
         {
-            if (searchedValue.Equals(currentNode.Value))
+            Node current = head;
+            if (current == null)
+                return;
+
+            if (current.Value.Equals(item))
             {
-                return currentNode;
+                if (current.Next != null)
+                    current = current.Next;
+                else
+                    current = null;
+
+                head = current;
+            }
+            else
+            {
+                while (current.Next != null && !current.Next.Value.Equals(item))
+                {
+                    current = current.Next;
+                }
+
+                if (current.Next != null && current.Next.Value.Equals(item))
+                    current.Next = current.Next.Next;
+            }
+        }
+
+        public bool Contains(T value)
+        {
+            return Find(value) != null;
+        }
+
+        public IEnumerable<Node> Iterate()
+        {
+            var currentNode = head;
+
+            while (currentNode != null)
+            {
+                yield return currentNode;
+
+                currentNode = currentNode.Next;
+            }
+        }
+
+        public void AddLast(T item)
+        {
+            var newNode = new Node(item);
+            InsertLast(newNode);
+        }
+
+        private void InsertLast(Node newNode)
+        {
+            if (tail is null)
+            {
+                head = tail = newNode;
+            }
+            else
+            {
+                InsertAfter(tail, newNode);
+            }
+        }
+
+        private void InsertAfter(Node existingNode, Node newNode)
+        {
+            if (existingNode is null)
+                head = newNode;
+            else
+                existingNode.Next = newNode;
+
+            tail = newNode;
+        }
+
+        private void InsertNode(Node newNode)
+        {
+            if (head != null)
+            {
+                head.Next = newNode;
+            }
+            else
+            {
+                head = newNode;
+            }
+        }
+
+        private Node Find(T searchedValue)
+        {
+            var currentNode = head;
+
+            while (currentNode != null)
+            {
+                if (searchedValue.Equals(currentNode.Value))
+                {
+                    return currentNode;
+                }
+
+                currentNode = currentNode.Next;
             }
 
-            currentNode = currentNode.Next;
+            return null;
         }
-
-        return null;
     }
-}
+} 
